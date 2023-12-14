668d9504fc953d86bb574d1fb385bfe9504eb5c0
Vivek <viveksah59@gmail.com>
2023-12-14 21:11:03 +0530

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Buttton from "./Buttton";
+import { Input } from "./input";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Postcard from "./Postcard";
+import PostForm from "./post-form";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select"
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Buttton,
+  Input,
+  RTE,
+  Signup,
+  Login,
+  Postcard,
+  PostForm,
+  AuthLayout,
+  Select,
+};
